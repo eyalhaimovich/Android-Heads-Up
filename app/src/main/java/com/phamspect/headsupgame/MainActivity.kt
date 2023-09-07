2c374e78ec209f402e8cf0452acd849b36f3f4f4
Phamda1340 <58050461+Phamda1340@users.noreply.github.com>
2023-09-07 12:06:56 -0700

    add category button functions

--- app/src/main/java/com/phamspect/headsupgame/MainActivity.kt
+++ app/src/main/java/com/phamspect/headsupgame/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var currentTime: TextView
     private lateinit var timer: CountDownTimer
     private  var isRunning = false
+    private lateinit var category: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +45,8 @@ class MainActivity : AppCompatActivity() {
                 //Say timer is already running
             }
         }
+
+        makeCategoryButtons()
         //end button listener
 //        stopButton.setOnClickListener {
 //            if(isRunning){
@@ -52,4 +55,52 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
     }
+
+    private fun makeCategoryButtons(){
+        setContentView(R.layout.activity_main)
+
+        var button:Button = findViewById(R.id.cat1)
+
+        button.setOnClickListener{
+            category = button.text.toString()
+        }
+        var button2 = findViewById<Button>(R.id.cat2)
+        button2.setOnClickListener{
+            category = button2.text.toString();
+        }
+        var button3 = findViewById<Button>(R.id.cat3)
+        button3.setOnClickListener{
+            category = button3.text.toString();
+        }
+        var button4 = findViewById<Button>(R.id.cat4)
+        button4.setOnClickListener{
+            category = button4.text.toString();
+        }
+        var button5 = findViewById<Button>(R.id.cat5)
+        button5.setOnClickListener{
+            category = button5.text.toString();
+
+        }
+        var button6 = findViewById<Button>(R.id.cat6)
+        button6.setOnClickListener{
+            category = button6.text.toString();
+            currentTime.text = button6.text.toString()
+        }
+        var button7 = findViewById<Button>(R.id.cat7)
+        button7.setOnClickListener{
+            category = button7.text.toString();
+            currentTime.text = button7.text.toString()
+        }
+        var button8 = findViewById<Button>(R.id.cat8)
+        button8.setOnClickListener{
+            category = button8.text.toString();
+            currentTime.text = button8.text.toString()
+        }
+        var button9 = findViewById<Button>(R.id.cat9)
+        button9.setOnClickListener{
+            category = button9.text.toString();
+            currentTime.text = button9.text.toString()
+        }
+    }
+
 }
