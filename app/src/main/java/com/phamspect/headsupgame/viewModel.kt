896a8e42bacbf7d0bcd80d51efacf757103e3f9b
Phamda1340 <58050461+Phamda1340@users.noreply.github.com>
2023-10-05 10:38:31 -0700

    # Conflicts: #	app/src/main/java/com/phamspect/headsupgame/MainActivity.kt #	app/src/main/res/layout/activity_main.xml

---
+++ app/src/main/java/com/phamspect/headsupgame/viewModel.kt
@@ -0,0 +1,48 @@
+package com.phamspect.headsupgame
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+class viewModel(private val savedStateHandle: SavedStateHandle): ViewModel() {
+    //vars
+    private var points: Int = 0
+    private var right: Int = 0
+    private var wrong: Int = 0
+    private var catsLiveData = MutableLiveData<List<Int>>()
+    //vars
+    //TODO make dictionary key:value cat1:listof(words)
+    fun setInput(points: Int, right: Int, wrong: Int) {
+        this.points = points
+        this.right = right
+        this.wrong = wrong
+    }
+
+    fun getPoints(): Int{
+        return points
+    }
+
+    fun getCatsLiveData(): MutableLiveData<List<Int>>{
+        return catsLiveData
+    }
+
+    fun updateCatsList(newList: List<Int>) {
+        catsLiveData.value = newList
+    }
+
+    fun right(){
+        right++
+        points++
+    }
+
+    fun wrong(){
+        wrong++
+        points--
+    }
+
+    fun reset(){
+        right = 0
+        wrong = 0
+        points = 0
+    }
+}
